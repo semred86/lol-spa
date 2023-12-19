6698154c101f96eeb7ac5ab8bde89e43fde2f149
semred86 <serafim.ryabykin@gmail.com>
2023-12-20 00:12:46 +0300

    Deploy lol spa website

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Champion } from "./pages/champion/Champion";
 import { Items } from "./pages/Items";
 import { Runes } from "./pages/Runes";
 import { Summoners } from "./pages/Summoners";
-import { Test } from "./pages/Test";
 
 import { NotFound } from "./pages/NotFound";
 import { useVersion } from "./hooks/useVersion";
@@ -24,7 +23,6 @@ function App() {
             <Route path="items" element={<Items />} />
             <Route path="runes" element={<Runes />} />
             <Route path="summoners" element={<Summoners />} />
-            <Route path="test" element={<Test />} />
             <Route path="notfound" element={<NotFound />} />
             <Route path="*" element={<NotFound />} />
           </Route>
