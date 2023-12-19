e5d43eaeab9123e3bb6711c59169daf39ad159b3
semred86 <serafim.ryabykin@gmail.com>
2023-12-20 00:31:11 +0300

    Deploy 3.0

--- src/main.tsx
+++ src/main.tsx
@@ -33,7 +33,7 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/lol-spa">
       <App />
     </BrowserRouter>
   </Provider>
