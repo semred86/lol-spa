6574aaba895117b0cd9e6310dd192b949458ecf7
semred86 <serafim.ryabykin@gmail.com>
2023-12-20 00:11:24 +0300

    Deploy lol spa website

--- src/hooks/useChampFilter.tsx
+++ src/hooks/useChampFilter.tsx
@@ -1,27 +1,26 @@
-import { IChamp } from "interfaces/champion/IChamp";
+import { IChamp } from "@/interfaces/champion/IChamp";
 import { useSelector } from "react-redux";
-import { RootState } from "store";
+import { RootState } from "@/store";
 import { useData } from "./useData";
-import { Champ } from "components/champion/Champ";
+import { Champ } from "@/components/champion/Champ";
 
 export function useChampFilter() {
-
-  let { data } = useData('champion').data
+  const { data } = useData("champion").data;
 
   const { name, tag } = useSelector((state: RootState) => state.champion);
 
   const regexp: RegExp = new RegExp(`^${name}|.+?('|\\s)${name}`, "i");
 
-  const champs: JSX.Element[] = []
+  const champs: JSX.Element[] = [];
 
   for (const name in data) {
     const champ: IChamp = data[name];
 
     if (
-      (name === '' || regexp.test(champ.name)) &&
-      (tag === 'All' || champ.tags.includes(tag))
+      (name === "" || regexp.test(champ.name)) &&
+      (tag === "All" || champ.tags.includes(tag))
     ) {
-      champs.push(<Champ champ={champ} key={name} />)
+      champs.push(<Champ champ={champ} key={name} />);
     }
   }
   return champs;
